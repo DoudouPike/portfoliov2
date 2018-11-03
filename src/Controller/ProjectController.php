cdab66d0b3f4019d7a9a2c15c98c9d5aa867b17c
Unknown <doudoupike@hotmail.fr>
2018-11-03 04:59:44 +0100

    Update template

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProjectController
+ * @package App\Controller
+ */
 class ProjectController extends AbstractController
 {
     /**
-     * @Route("/projects", name="projects_list")
+     * @Route("/", name="index")
      */
     public function index()
     {
